dac3aaf4150da1ba8a5c9c54b64ecc001a0e3f23
Bruno Croci <crocidb@gmail.com>
2024-06-14 02:17:24 +0200

    changing the content type

--- app/Resolve.hs
+++ app/Resolve.hs
@@ -8,18 +8,24 @@ import Request
 import Data.List.Split
 import Data.List (intercalate)
 
-data Response = OK String | NotFound
+data ContentType = TextPlain | ApplicationOctetStream
+data Response = OK String ContentType | NotFound
 
 httpStatus :: Response -> String
 httpStatus NotFound = "HTTP/1.1 404 Not Found\r\n"
-httpStatus (OK _) = "HTTP/1.1 200 OK\r\n"
+httpStatus (OK _ _) = "HTTP/1.1 200 OK\r\n"
 
-getContentHeader :: String -> String
-getContentHeader body = "Content-Type: text/plain\r\nContent-Length: " ++ show (length body) ++ "\r\n\r\n"
+contentType :: ContentType -> String
+contentType TextPlain = "text/plain"
+contentType ApplicationOctetStream = "application/octet-stream"
+
+getContentHeader :: Response -> String
+getContentHeader NotFound = ""
+getContentHeader (OK body ctype) = "Content-Type: " ++ contentType ctype ++ "\r\nContent-Length: " ++ show (length body) ++ "\r\n\r\n"
 
 returnBody :: Response -> String
 returnBody NotFound = httpStatus NotFound ++ "\r\n"
-returnBody (OK body) = httpStatus (OK body) ++ getContentHeader body ++ body
+returnBody (OK body ct) = httpStatus (OK body ct) ++ getContentHeader (OK body ct) ++ body
 
 resolveRequest :: Request -> String -> IO Response
 resolveRequest request path = do
@@ -28,11 +34,11 @@ resolveRequest request path = do
     let url_value = if length url_components > 2 then url_components !! 2 else ""
 
     case url_endpoint of
-        "echo" -> do return (OK url_value)
-        "user-agent" -> do return (OK $ userAgent request)
+        "echo" -> do return (OK url_value TextPlain)
+        "user-agent" -> do return (OK (userAgent request) TextPlain)
         "files" -> do
             filecontents <- openFileAsString path (intercalate "/" (drop 2 url_components))
             case filecontents of
                 Nothing -> do return NotFound
-                (Just content) -> do return (OK content)
+                (Just content) -> do return (OK content ApplicationOctetStream)
         _ -> do return NotFound
