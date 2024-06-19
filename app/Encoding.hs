0092f8a15804567414a60b3cc697cfd1983804ad
Bruno Croci <crocidb@gmail.com>
2024-06-19 22:21:26 +0200

    picking an encoding from a list of possible

--- app/Encoding.hs
+++ app/Encoding.hs
@@ -2,6 +2,8 @@
 
 module Encoding where
 
+import Data.List.Split
+
 data EncodingType = None | Gzip deriving (Show, Eq)
 
 encodingToString :: EncodingType -> String
@@ -12,3 +14,9 @@ stringToEncoding :: String -> EncodingType
 stringToEncoding "gzip" = Gzip
 stringToEncoding "" = None
 stringToEncoding _ = None
+
+stringsToEncoding :: String -> EncodingType
+stringsToEncoding str =
+    if any (\s -> stringToEncoding s == Gzip) $ splitOn ", " str
+        then Gzip
+        else None
